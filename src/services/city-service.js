809b8bb1fcd6a372613fdc4ffa30ec2230267c6b
Lakshay Bansal <lakshaybansal1905@gmail.com>
2025-04-06 22:00:52 +0530

    added get all cities api

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -40,6 +40,15 @@ class CityService{
             throw {error};
         }
     }
+    async getAllCities(){
+        try{
+            const cities=await this.cityRepository.getAllCities();
+            return cities;
+        }catch(error){
+            console.log("Something went wrong at the servie layer");
+            throw {error};
+        }
+    }
    
 }
 module.exports=CityService;
